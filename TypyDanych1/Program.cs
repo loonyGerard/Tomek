be283058b57185fd160a53d93f1f631c3ed7df37
loonyGerard <114941032+loonyGerard@users.noreply.github.com>
2022-11-29 12:27:12 +0100

    update 29_11

--- TypyDanych1/Program.cs
+++ TypyDanych1/Program.cs
@@ -9,10 +9,11 @@ void Zadanie1()
 
     while (liczba <= 100)
     {
-        System.Console.Write(liczba +", ");
+        System.Console.Write(liczba + ", ");
         liczba++;
     }
 }
+
 //-----------------------------------------------------
 //-----------------------------------------------------
 //-----------------------------------------------------
@@ -33,16 +34,18 @@ void Zadanie2()
         }
         liczba++;
     }
-    System.Console.Write(licznik +", ");
+    System.Console.Write(licznik + ", ");
 }
+
 //-----------------------------------------------------
 //-----------------------------------------------------
 //-----------------------------------------------------
 
-void Zadanie3(){
+void Zadanie3()
+{
     System.Console.WriteLine(" ");
     System.Console.WriteLine(" ");
-        System.Console.WriteLine("Zadanie 1.3");
+    System.Console.WriteLine("Zadanie 1.3");
     int liczba = -100;
     int licznik = 0;
 
@@ -56,14 +59,16 @@ void Zadanie3(){
     }
     System.Console.Write(licznik);
 }
+
 //-----------------------------------------------------
 //-----------------------------------------------------
 //-----------------------------------------------------
 
-void Zadanie4(){
+void Zadanie4()
+{
     System.Console.WriteLine();
     System.Console.WriteLine();
-        System.Console.WriteLine("Zadanie 1.4");
+    System.Console.WriteLine("Zadanie 1.4");
     int liczba = -000;
     int licznik = 0;
 
@@ -77,6 +82,7 @@ void Zadanie4(){
     }
     System.Console.WriteLine(licznik);
 }
+
 //-----------------------------------------------------
 //-----------------------------------------------------
 //-----------------------------------------------------
@@ -88,29 +94,23 @@ void Zadanie5()
     System.Console.WriteLine();
     System.Console.WriteLine("Zadanie 1.5");
     int liczba = -100;
-    int dzielnik =8;
-    int modulo = liczba%dzielnik;
-    bool moduloBool = true;
-    
-
+    int dzielnik = 8;
     while (liczba <= 100)
     {
-        if(modulo <4)
+        int a = liczba % dzielnik;
+        if (a < 0)
         {
-            moduloBool=false;
+            a = -a;
         }
-        if (moduloBool)
+        if (a > 4)
         {
-            liczba = liczba*modulo;
-        
-        System.Console.WriteLine($"!! {liczba} !!");
-        liczba++;
+            System.Console.WriteLine($"!! {liczba * a} !!");
         }
         else
         {
-            System.Console.Write(liczba +", ");
-        liczba++;
+            System.Console.Write(liczba + ", ");
         }
+        liczba++;
     }
     System.Console.WriteLine();
     System.Console.WriteLine("Koniec zadania 1.5.");
@@ -128,11 +128,13 @@ void Zadanie6()
     System.Console.WriteLine("Zadanie 1.6");
     double liczba = 0;
 
-    while (liczba <= 10){
+    while (liczba <= 10)
+    {
         System.Console.Write($"{Math.Round(liczba, 1)}  ");
         liczba += 0.1;
     }
 }
+
 //-----------------------------------------------------
 //-----------------------------------------------------
 //-----------------------------------------------------
@@ -141,31 +143,28 @@ void Zadanie7()
 {
     System.Console.WriteLine();
     System.Console.WriteLine();
-System.Console.WriteLine("Zadanie 1.7");
-int liczba = 100;
-        bool[] spr = new bool[liczba+1];
-        for(int i = 2; i<=liczba; i++)
-        {
-            spr[i]=true;
+    System.Console.WriteLine("Zadanie 1.7");
+    int max = 100;
+    
+    for(int i = 1; i <= max; i++){
+        if(Dzielniki(i) == 2) {
+            System.Console.WriteLine(i);
         }
-        for(int i = 2; i<=liczba; i++)
-        {
-            if (spr[i])
-            {
-                for (int j = i*2; j<=liczba; j+=i)
-                {
-                    spr[j] = false;
-                }
-            }
+    }
+}
+
+int Dzielniki(int num) {
+    int liczbaDzielnikow = 0;
+    int dzielnik = num;
+    while(dzielnik > 0) {
+        if(num % dzielnik == 0) {
+            liczbaDzielnikow++;
         }
-        System.Console.WriteLine("Liczby pierwsze w przedziale do " + liczba + ":");
-        // for (int i=0; i<=liczba; i++);
-        // {
-        //     if (spr[i])
-        //     {
-        //         System.Console.Write(i+", ");
-        //     }
-        // }
+
+        dzielnik--;
+    }
+
+    return liczbaDzielnikow;
 }
 
 //-----------------------------------------------------
@@ -176,48 +175,47 @@ void Zadanie8()
 {
     System.Console.WriteLine();
     System.Console.WriteLine();
-System.Console.WriteLine("Zadanie 1.8");
+    System.Console.WriteLine("Zadanie 1.8");
     //TYPY CALKOWITE
-//sbyte 8 bit
-Console.WriteLine($"sbyte od {sbyte.MinValue} do {sbyte.MaxValue} rozmiar 8 bit");
+    //sbyte 8 bit
+    Console.WriteLine($"sbyte od {sbyte.MinValue} do {sbyte.MaxValue} rozmiar 8 bit");
 
-//byte  8 bit
-Console.WriteLine($"byte od {byte.MinValue} do {byte.MaxValue} rozmiar 8 bit");
+    //byte  8 bit
+    Console.WriteLine($"byte od {byte.MinValue} do {byte.MaxValue} rozmiar 8 bit");
 
-//short 16 bit
-Console.WriteLine($"short od {short.MinValue} do {short.MaxValue} rozmiar 16 bit");
+    //short 16 bit
+    Console.WriteLine($"short od {short.MinValue} do {short.MaxValue} rozmiar 16 bit");
 
-//ushort 16 bit
-Console.WriteLine($"ushort od {ushort.MinValue} do {ushort.MaxValue} rozmiar 16 bit");
+    //ushort 16 bit
+    Console.WriteLine($"ushort od {ushort.MinValue} do {ushort.MaxValue} rozmiar 16 bit");
 
-//int 32 bit
-Console.WriteLine($"int od {int.MinValue} do {int.MaxValue} rozmiar 32 bit");
+    //int 32 bit
+    Console.WriteLine($"int od {int.MinValue} do {int.MaxValue} rozmiar 32 bit");
 
-//uint 32 bit
-Console.WriteLine($"uint od {uint.MinValue} do {uint.MaxValue} rozmiar 32 bit");
+    //uint 32 bit
+    Console.WriteLine($"uint od {uint.MinValue} do {uint.MaxValue} rozmiar 32 bit");
 
-//long 64 bit
-Console.WriteLine($"long od {long.MinValue} do {long.MaxValue} rozmiar 64 bit");
+    //long 64 bit
+    Console.WriteLine($"long od {long.MinValue} do {long.MaxValue} rozmiar 64 bit");
 
-//ulong 64 bit
-Console.WriteLine($"ulong od {ulong.MinValue} do {ulong.MaxValue} rozmiar 8 bit");
+    //ulong 64 bit
+    Console.WriteLine($"ulong od {ulong.MinValue} do {ulong.MaxValue} rozmiar 8 bit");
 
-//nint 32/64 bit
-Console.WriteLine($"nint od {nint.MinValue} do {nint.MaxValue} rozmiar 32/64 bit");
+    //nint 32/64 bit
+    Console.WriteLine($"nint od {nint.MinValue} do {nint.MaxValue} rozmiar 32/64 bit");
 
-//nuint 32/64 bit
-Console.WriteLine($"nuint od {nuint.MinValue} do {nuint.MaxValue} rozmiar 32/64 bit");
+    //nuint 32/64 bit
+    Console.WriteLine($"nuint od {nuint.MinValue} do {nuint.MaxValue} rozmiar 32/64 bit");
 
-//float
-Console.WriteLine($"float od {float.MinValue} do {float.MaxValue} rozmiar 4 bit");
+    //float
+    Console.WriteLine($"float od {float.MinValue} do {float.MaxValue} rozmiar 4 bit");
 
-//double 
-Console.WriteLine($"double od {double.MinValue} do {double.MaxValue} rozmiar 8 bit");
+    //double
+    Console.WriteLine($"double od {double.MinValue} do {double.MaxValue} rozmiar 8 bit");
 
-//decimal
-Console.WriteLine($"decimal od {decimal.MinValue} do {decimal.MaxValue} rozmiar 8 bit");
+    //decimal
+    Console.WriteLine($"decimal od {decimal.MinValue} do {decimal.MaxValue} rozmiar 8 bit");
 }
-
 
 Zadanie1();
 Zadanie2();
