24b5fa6c7795df28c68ed2a5149150db642110b9
loonyGerard <114941032+loonyGerard@users.noreply.github.com>
2022-11-30 13:06:03 +0100

    elo

---
+++ TypyDanych3/Program.cs
@@ -0,0 +1,67 @@
+﻿int x = 3;
+int a = 1;
+
+System.Console.WriteLine();
+System.Console.WriteLine("zadanir 3.1");
+int result1 = ((-x * -x * -x) + 2 * (x * x) - a * x - 4);
+System.Console.WriteLine(result1);
+
+System.Console.WriteLine();
+System.Console.WriteLine("zadanir 3.2");
+int result2 = 9 * (x * x * x * x) - 12 * (x * x) + 4;
+System.Console.WriteLine(result2);
+
+System.Console.WriteLine();
+System.Console.WriteLine("zadanir 3.3");
+int result3 = 9 * (4 * 4 * 4 * 4) + 12 * (x * x) + 4;
+System.Console.WriteLine(result3);
+
+System.Console.WriteLine();
+System.Console.WriteLine("zadanir 3.4");
+int result4 = 9 * (x * x * x * x) - 4;
+System.Console.WriteLine(result4);
+
+System.Console.WriteLine();
+System.Console.WriteLine("zadanir 3.5");
+int result5 = 9 * (x * x * x * x) + 4;
+System.Console.WriteLine(result5);
+
+System.Console.WriteLine();
+System.Console.WriteLine("zadanir 3.6");
+int result6 = (x * x) - 4 * x + 4;
+System.Console.WriteLine(result6);
+
+System.Console.WriteLine();
+System.Console.WriteLine("zadanir 3.7");
+int result7 = (x * x) + 2 * x + 4;
+System.Console.WriteLine(result7);
+
+System.Console.WriteLine();
+System.Console.WriteLine("zadanir 3.8");
+int result8 = (1 - x) * (x + 2) * (x * x - 4);
+System.Console.WriteLine(result8);
+
+System.Console.WriteLine();
+System.Console.WriteLine("zadanir 3.9");
+int result9 = (x - 3) * (x + 3) * (x * x - 9);
+System.Console.WriteLine(result9);
+
+System.Console.WriteLine();
+System.Console.WriteLine("zadanir 3.10");
+int result10 = (x * x + 1) * (x * x + 4) * (x * x + 3);
+System.Console.WriteLine(result10);
+
+System.Console.WriteLine();
+System.Console.WriteLine("zadanir 3.11");
+int result11 = (2 - x) * (x + 3) * (x * x - 1);
+System.Console.WriteLine(result11);
+
+System.Console.WriteLine();
+System.Console.WriteLine("zadanir 3.12");
+int result12 = (x * x * x) - 3 * (x * x) + 5 * x - 15;
+System.Console.WriteLine(result12);
+
+System.Console.WriteLine();
+System.Console.WriteLine("zadanir 3.13");
+int result13 = 2 * (x * x * x) - (x * x) - 6 * x + 3;
+System.Console.WriteLine(result13);
